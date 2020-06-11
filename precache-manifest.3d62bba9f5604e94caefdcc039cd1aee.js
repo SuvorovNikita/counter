2eb6b5da2562f56b7f1ec6e00d7754fc4e0443cc
Никита <nikit.suvorov@gmail.com>
2020-06-11 18:11:37 +0300

    Updates

---
+++ precache-manifest.3d62bba9f5604e94caefdcc039cd1aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8769aab4d3bff73da94aca0b0c18897",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "73908b88bc4a15470d8d",
+    "url": "/counter/static/css/main.433d7536.chunk.css"
+  },
+  {
+    "revision": "37aa91f0bc9c3a95a434",
+    "url": "/counter/static/js/2.d8372a4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.d8372a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73908b88bc4a15470d8d",
+    "url": "/counter/static/js/main.66151566.chunk.js"
+  },
+  {
+    "revision": "890e0606e2c9e34496c7",
+    "url": "/counter/static/js/runtime-main.01fdcc42.js"
+  }
+]);
